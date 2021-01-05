a3bf8c55e37744a1af5a4a77b431d4bd93d6d418
zouzhangwei <wearecisco@gmail.com>
2021-01-05 15:42:42 +0800

    update color list for flutter dart

--- flutter/color_list/lib/main.dart
+++ flutter/color_list/lib/main.dart
@@ -5,25 +5,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Color List',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -33,16 +20,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -50,68 +27,198 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+// 进度监听对象
+  final ValueNotifier<double> factor = ValueNotifier<double>(1 / 5);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+// 页数监听对象
+  final ValueNotifier<int> page = ValueNotifier<int>(1);
+
+  // 页面滑动控制器
+  PageController _ctrl;
+
+// 测试组件 色块
+  final List<Widget> testWidgets =
+      // [Colors.red, Colors.yellow, Colors.blue, Colors.green, Colors.orange]
+      [
+    ColorObject(name: "红色", color: Colors.red),
+    ColorObject(name: "黄色", color: Colors.yellow),
+    ColorObject(name: "蓝色", color: Colors.blue),
+    ColorObject(name: "绿色", color: Colors.green),
+    ColorObject(name: "橙色", color: Colors.orange),
+  ]
+          .map(
+            (e) => Container(
+              decoration: BoxDecoration(
+                color: e.color,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  e.name,
+                  style: TextStyle(fontSize:30.0, color: Colors.white),
+                ),
+              ),
+            ),
+          )
+          .toList();
+
+  Color get startColor =>  Color(0xff00fa60);//Colors.red; // 起点颜色
+  Color get endColor => Color(0xff0575e6);//Colors.blue; // 终点颜色
+
+
+  //圆角装饰
+  BoxDecoration get boxDecoration => const BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20), topRight: Radius.circular(20)));
+
+  // 初始化
+  @override
+  void initState() {
+    super.initState();
+    _ctrl = PageController(
+      viewportFraction: 0.9,
+    )..addListener(() {
+        double value = (_ctrl.page + 1) % 5 / 5;
+        factor.value = value == 0 ? 1 : value;
+      });
+  }
+
+  // 释放对象
+  @override
+  void dispose() {
+    _ctrl.dispose();
+    page.dispose();
+    factor.dispose();
+    super.dispose();
+  }
+
+  Widget _buildProgress() => Container(
+        margin: EdgeInsets.only(bottom: 12, left: 48, right: 48, top: 10),
+        height: 2,
+        child: ValueListenableBuilder(
+          valueListenable: factor,
+          builder: (context, value, child) {
+            return LinearProgressIndicator(
+              value: factor.value,
+              valueColor: AlwaysStoppedAnimation(
+                Color.lerp(
+                  startColor,
+                  endColor,
+                  factor.value,
+                ),
+              ),
+            );
+          },
+        ),
+      );
+
+  Widget _buildTitle(BuildContext context) {
+    print('---------_buildTitle------------');
+    return Container(
+      alignment: Alignment.center,
+      height: MediaQuery.of(context).size.height * 0.25,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            Icons.api,
+            color: Colors.white,
+            size: 45,
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          ValueListenableBuilder(
+            valueListenable: page,
+            builder: _buildWithPageChange,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildWithPageChange(BuildContext context, int value, Widget child) {
+    return Text(
+      "颜色 $value/5",
+      style: TextStyle(fontSize: 30, color: Colors.white),
+    );
+  }
+
+  Widget _buildContent() {
+    return Container(
+        padding: EdgeInsets.only(bottom: 80, top: 40),
+        child: Column(
+          children: [
+            Expanded(
+              child: PageView.builder(
+                onPageChanged: (index) => page.value = index + 1,
+                controller: _ctrl,
+                itemCount: testWidgets.length,
+                itemBuilder: (_, index) => AnimatedBuilder(
+                    child: Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: testWidgets[index],
+                    ),
+                    animation: _ctrl,
+                    builder: (context, child) =>
+                        _buildAnimOfItem(context, child, index)),
+              ),
+            ),
+            _buildProgress(),
+          ],
+        ));
+  }
+
+  Widget _buildAnimOfItem(BuildContext context, Widget child, int index) {
+    double value;
+    if (_ctrl.position.haveDimensions) {
+      value = _ctrl.page - index;
+    } else {
+      value = index.toDouble();
+    }
+    value = (1 - ((value.abs()) * .5)).clamp(0, 1).toDouble();
+    value = Curves.easeOut.transform(value);
+    return Transform(
+      transform: Matrix4.diagonal3Values(1.0, value, 1.0),
+      alignment: Alignment.center,
+      child: child,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: ValueListenableBuilder(
+        valueListenable: factor,
+        builder: (_, value, child) => Container(
+          color: Color.lerp(startColor, endColor, value),
+          child: child, //<--- tag1
+        ),
+        child: Container(
+          //<--- tag2
+          child: Column(
+            children: [
+              _buildTitle(context),
+              Expanded(
+                  child: Container(
+                child: _buildContent(),
+                margin: const EdgeInsets.only(left: 8, right: 8),
+                decoration: boxDecoration,
+              ))
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class ColorObject {
+  ColorObject({this.name, this.color});
+
+  final String name;
+  final Color color;
 }
